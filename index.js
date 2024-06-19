8e5f0ced5d2c5c1b0c2612bb5d0f9695a5377ad0
eustachekamala <eustachejared25@gmail.com>
2024-06-19 22:22:22 +0300

    I used a event of keydown to I try to create a play for move the div in left or right when we click the button of direction on the keyboard

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.background = "#FF69B4";
+// dodger.style.bottom = "0px";
+// dodger.style.left = "0px";
+
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px","");
+    const left = parseInt(leftNumbers,10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px","");
+    const left = parseInt(leftNumbers,10);
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", (e) => {
+    if(e.key === "ArrowLeft"){
+        mouseDodgerLeft();
+    }
+});
+
+document.addEventListener("keydown", (ev) => {
+    if (ev.key === "ArrowRight") {
+    mouseDodgerRight();
+    }
+});
